fdf21b6ff1efa224acc58197c858e23eff2bf63f
Deepak Singh <iamdeepaksinghh@gmail.com>
2022-02-21 10:29:16 +0530

    Updates main.js to load current experience in home page.
    Updates main.js to load current experience in home page.

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,8 @@ const intern2comp = 'Max Secure Softwares';
 const internship1 = 'Mobile Security';
 const intern1role = 'Summer Research Intern';
 const intern1comp = 'IIT Roorkee';
-exp.innerHTML = `My ${degree} thesis project was <b>${thesisProject}</b>.`
+exp.innerHTML = `I am working as a SDE in Jio Health Hub. My responsibilities include developing microservices to enable scalable backend<br>`
+	+ `pipelines to ensure high availablity and consistency. I work with technologies like Python, FastAPI, NoSQL DB and Apache Kafka.<br>`
+            + `My ${degree} thesis project was <b>${thesisProject}</b>.`
             + `<br>I worked on <b>${internship2}</b> as ${intern2role} at ${intern2comp}.`
             + `<br>I worked on <b>${internship1}</b> as ${intern1role} at ${intern1comp}.`
